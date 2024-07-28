21c56ddc0257380d8e2fd14c5736b598d0d37f2d
TLLRSK <tullarisk@gmail.com>
2024-07-28 13:35:11 +0200

    change: lazyImage functions

--- src/components/lazyImage/LazyImage.jsx
+++ src/components/lazyImage/LazyImage.jsx
@@ -1,5 +1,7 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 import getImage from "../../js/utils";
+import { usePageTransition } from "../../js";
 
 const LazyImage = (props) => {
     const {imageUrl, mediaData, page} = props;
@@ -8,42 +10,65 @@ const LazyImage = (props) => {
     const medium =getImage(imageUrl, mediaData).media_details?.sizes?.medium?.source_url;
     const large =getImage(imageUrl, mediaData).media_details?.sizes?.large?.source_url;
     const full = getImage(imageUrl, mediaData)?.source_url;
+    const woocommerceSingle = getImage(imageUrl, mediaData)?.media_details?.sizes?.woocommerce_single?.source_url;
 
-
-    const getClassName = (page) => {
+    const getClassName = () => {
         switch(page) {
             case "home":
-                return "img--home";
+                return `img--home`;
             case "single-work":
-                return "img--single-work";
+                return `img--single-work`;
             default:
                 return "img";
         }
     }
 
-    const srcSet = [
-        thumbnail && `${thumbnail} 360w,`,
-        medium && `${medium} 720w,`,
-        large && `${large} 1280w,` && `${full} 1280w,`,
-        full && `${full} 1920w,`
-    ].join(' ')
+    const getContainerClassName = () => {
+        switch(page) {
+            case "home":
+                return `img__container--home`;
+            default:
+                return "img__container";
+        }
+    }
+
+    const getSrcSet = () => {
+        switch(page) {
+            case "home":
+                return [
+                    thumbnail && `${thumbnail} 360w,`,
+                    medium && `${medium} 720w,`,
+                    large && `${large} 1280w,`,
+                ];
+            case "single-work":
+                return [
+                    thumbnail && `${thumbnail} 360w,`,
+                    medium && `${medium} 720w,`,
+                    woocommerceSingle && `${woocommerceSingle} 1280w,`,
+                ];
+        }
+    }
 
     const commonProps = {
-        src: medium,
-        srcSet: srcSet,
+        src: full,
+        srcSet: getSrcSet(),
         alt: getImage(imageUrl, mediaData)?.alt_text || "",
         className: getClassName(page),
-        threshold: 1200,
-        placeholderSrc: thumbnail,
+        threshold: 800,
+        effect: 'opacity',
+        wrapperClassName: getContainerClassName(),
+        wrapperProps:{
+            style: {transition: "opacity .4s ease-in-out"},
+        },
     }
 
     if (page === 'single-work') {
         commonProps.onClick = (() => window.open(full));
     }
     
-    return <figure className="img__container">
-        <LazyLoadImage {...commonProps}/>
-    </figure>
+    return (
+            <LazyLoadImage {...commonProps} key={imageUrl} />
+    );
 }
 
 export default LazyImage;
